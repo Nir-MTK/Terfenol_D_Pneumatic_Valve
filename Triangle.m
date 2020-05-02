6fd33e33209427ab52614ff19a3689776af984f9
Nir <nir.mtk@gmail.com>
2020-05-02 21:00:33 +0300

    20-80
    change the triangle angle range

--- Triangle.m
+++ Triangle.m
@@ -4,7 +4,8 @@ close all
 clc
 %% Paramaters
 deformation = 0.1; %[mm] Plunger maximum movement
-angles_vec = linspace(10,80,8); % [deg] 
+angles_vec = [80 70 60 50 40 30 20]; % [deg] 
+leg_vec = ['b' 'k' 'r' 'g' 'y' 'c' 'm'];
 for j=1:length(angles_vec)
 w = angles_vec(j); %[deg] Half traingle top angle 
 deg=deg2rad(w); 
@@ -15,10 +16,16 @@ c = [x*cos(deg),0];% Triangle 3rd edge
 base = linspace(0,max(b(1)));
 line = polyfit([a(1),b(1)],[a(2),b(2)],1);
 
-% figure() %Plotting triangle shape
-% plot(base,polyval(line,base))
-% hold on
-% plot(base,-1*polyval(line,base))
+figure(1) %Plotting triangle shape
+color = leg_vec(j);
+plot(base,polyval(line,base),color)
+hold on
+plot(base,-1*polyval(line,base),color)
+hold on
+plot(0.1*ones(1,100),linspace(0,b(2)),color)
+hold on
+plot(0.1*ones(1,100),-1*linspace(0,b(2)),color)
+hold on
 
 tri_func = @(d) line(1).*d + line(2);
 
@@ -26,7 +33,8 @@ tri_func = @(d) line(1).*d + line(2);
      tri_area(i) = integral(tri_func,0,base(i));
      
  end
- plot(base,2.*tri_area)
+ figure(2)
+ plot(base,2.*tri_area,color)
  hold on
 end
 % hold off
@@ -35,9 +43,4 @@ ylabel('Tap Area [mm^2]')
 grid on
 title('Triangular Tap Shape Comperration')
 
-% legend('10^{\circ}','20^{\circ}','30^{\circ}','40^{\circ}','50^{\circ}','60^{\circ}','70^{\circ}','80^{\circ}' ,'location','northwest')
-
-run('Circular.m')
-plot(x,area)
-% hold on
-% plot(base,2.*tri_area)
+legend('80^{\circ}','70^{\circ}','60^{\circ}','50^{\circ}','40^{\circ}','30^{\circ}','20^{\circ}','location','northwest')
