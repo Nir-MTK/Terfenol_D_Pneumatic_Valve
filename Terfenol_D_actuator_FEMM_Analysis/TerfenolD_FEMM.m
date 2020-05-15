621312c2cec4994737bfd9eaaa340035a0dc2da2
Nir <nir.mtk@gmail.com>
2020-05-15 15:37:06 +0300

    Auto Analysis complete!
    need to add mo_blockintegral for all data

--- Terfenol_D_actuator_FEMM_Analysis/TerfenolD_FEMM.m
+++ Terfenol_D_actuator_FEMM_Analysis/TerfenolD_FEMM.m
@@ -5,11 +5,21 @@ format compact
 close all
 clc
 CircName = 'Coil';
+%% 
+i_vec =0:0.25:10 ;
 %% Open existing problem
 openfemm;
+
 opendocument('H:\Terfenol_D_Pneumatic_Valve\FEMM\Terfenol_Solenoid_2.FEM') %load FEMM problem
-mi_setcurrent(CircName,10) %Set the current to 0
+for i = 1:length(i_vec)
+mi_setcurrent(CircName,i_vec(i)) %Set the current to 0
 mi_analyze(0)
 mi_loadsolution
 mo_selectblock(15,50)
-mo_blockintegral(2)
+AJ_sol = mo_blockintegral(0); 
+A_sol(i) = mo_blockintegral(1);
+AL_sol(i) =  mo_blockintegral(2);
+RL_sol(i) =  mo_blockintegral(3);
+end
+
+plot(i_vec,A_sol)
